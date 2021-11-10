a93ce1bd2d0e4c55c46a2271a66d4756231c53c8
Debraj Kundu <kundudebraj4272@gmail.com>
2021-11-11 02:00:35 +0530

    added support for delimiter change

--- string-calculator/src/main/java/com/example/App.java
+++ string-calculator/src/main/java/com/example/App.java
@@ -15,10 +15,17 @@ public final class App {
     }
 
     public static int Add(String numbers) {
+        String delimiter = null;
         if (numbers.isBlank())
             return 0; // Input Blank
-        if (numbers.contains(",")) {
-            String nums[] = numbers.split("[,\n]");
+        if (numbers.startsWith("//")) {
+            delimiter = numbers.substring(0, numbers.indexOf("\n"));
+            numbers = numbers.substring(numbers.indexOf("\n") + 1);
+            delimiter = "" + delimiter.charAt(delimiter.length() - 1);
+        }
+        if (numbers.contains(",") || numbers.contains("\n") || ((delimiter != null) && numbers.contains(delimiter))) {
+            String regex = "[,\n" + delimiter + "]";
+            String nums[] = numbers.split(regex);
             int sum = 0;
             for (String n : nums) {
                 sum += Integer.parseInt(n);
