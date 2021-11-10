d4ebe14e02ce572f077dd3a5197aba0484a9e6ec
Debraj Kundu <kundudebraj4272@gmail.com>
2021-11-11 00:44:03 +0530

    First test Passed

--- string-calculator/src/main/java/com/example/App.java
+++ string-calculator/src/main/java/com/example/App.java
@@ -13,4 +13,10 @@ public final class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+
+    public static int Add(String numbers) {
+        if(numbers.isBlank()) return 0;
+
+        return -1;
+    }
 }
